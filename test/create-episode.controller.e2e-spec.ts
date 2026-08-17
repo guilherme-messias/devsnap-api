452b40bcd3264f981498db7718c7c6f29f393866
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-17 09:03:07 -0300

    refactor: remove old E2E tests and add new service files for episode management

--- test/create-episode.controller.e2e-spec.ts
+++ test/create-episode.controller.e2e-spec.ts
@@ -1,8 +1,8 @@
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
 import { Test } from '@nestjs/testing';
-import { AppModule } from '../../app.module';
-import { PrismaService } from '../../prisma/prisma.service';
+import { PrismaService } from '../src/prisma/prisma.service';
+import { AppModule } from '../src/app.module';
 
 describe('Create Episode (E2E)', () => {
   let app: INestApplication;
