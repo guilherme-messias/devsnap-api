43bbf43d8407ef3c1f5b66a00d2f406004931cd0
Guilherme Messias <guilherme.messias@sistematech.tech>
2026-08-20 16:33:53 -0300

    feat: add service to mark episodes as reviewed

---
+++ src/episodes/services/mark-episode-as-reviewed.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class MarkEpisodeAsReviewedService {
+  constructor(private prisma: PrismaService) {}
+  async markEpisodeAsReviewed(id: string, reviewed: boolean) {
+    const { count } = await this.prisma.episode.updateMany({
+      where: { id },
+      data: { reviewed },
+    });
+    if (count === 0) {
+      return null;
+    }
+
+    return this.prisma.episode.findUnique({
+      where: { id },
+    });
+  }
+}
