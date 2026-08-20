02ae29fb7c87bd5121016889e3a5b2120f59c06b
Guilherme Messias <guilherme.messias@sistematech.tech>
2026-08-20 18:35:52 -0300

    feat: add swagger decorators

--- src/episodes/controllers/create-episode.controller.ts
+++ src/episodes/controllers/create-episode.controller.ts
@@ -5,13 +5,26 @@ import {
 } from './schemas/episode.type';
 import { ZodValidationPipe } from '../../pipes/ZodValidationPipe';
 import { CreateEpisodeService } from '../services/create-episode.service';
+import { ApiOperation, ApiTags, ApiResponse } from '@nestjs/swagger';
 
+@ApiTags('episodes')
 @Controller('/episodes')
 export class CreateEpisodeController {
   constructor(private readonly createEpisodeService: CreateEpisodeService) {}
 
   @Post()
   @HttpCode(201)
+  @ApiOperation({
+    summary: 'Create a new episode',
+  })
+  @ApiResponse({
+    status: 201,
+    description: 'The episode has been successfully created.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Validation failed',
+  })
   @UsePipes(new ZodValidationPipe(createEpisodeSchema))
   async createEpisode(@Body() body: CreateEpisodeRequest) {
     const { title, stack, error, solution } = body;
