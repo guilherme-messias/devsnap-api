c37cfbb76e32638bb2c3a34c0b526a05bca6f848
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-21 09:36:45 -0300

    refactor: add validation constraints to create and update episode schemas

--- src/episodes/controllers/schemas/request/update-episode.request.schema.ts
+++ src/episodes/controllers/schemas/request/update-episode.request.schema.ts
@@ -2,10 +2,10 @@ import z from 'zod';
 
 export const updateEpisodeSchema = z
   .object({
-    title: z.string(),
-    stack: z.string(),
-    error: z.string(),
-    solution: z.string(),
+    title: z.string().trim().min(1).max(150),
+    stack: z.string().trim().min(1).max(100),
+    error: z.string().trim().min(1).max(5000),
+    solution: z.string().trim().min(1).max(5000),
   })
   .partial();
 
