4caa4f1ddd028267ba96fe3552e67a7c24bcb6ef
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-21 10:29:45 -0300

    feat: add Swagger documentation for the recent episodes search endpoint

--- src/episodes/controllers/fetch-recent-episodes.controller.ts
+++ src/episodes/controllers/fetch-recent-episodes.controller.ts
@@ -2,6 +2,7 @@ import { Controller, HttpCode, Get, Query } from '@nestjs/common';
 import z from 'zod';
 import { ZodValidationPipe } from '../../pipes/ZodValidationPipe';
 import { FetchRecentEpisodesService } from '../services/fetch-recent-episodes.service';
+import { ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 const pageQueryParamsSchema = z
   .string()
@@ -12,6 +13,8 @@ const pageQueryParamsSchema = z
 
 const queryValidationPipe = new ZodValidationPipe(pageQueryParamsSchema);
 type PageQueryParams = z.infer<typeof pageQueryParamsSchema>;
+
+// @ApiTags('episodes')
 @Controller('/episodes')
 export class FetchRecentEpisodesController {
   constructor(
@@ -20,6 +23,36 @@ export class FetchRecentEpisodesController {
 
   @Get()
   @HttpCode(200)
+  //       @ApiOperation({
+  //         summary: 'Fetch recent episodes',
+  //       })
+
+  //     @ApiQuery({
+  //       name: 'page',
+  //       description: 'Page number for pagination (default: 1)',
+  //       required: false,
+  //       schema: {
+  //         type: 'integer',
+  //         default: 1,
+  //         minimum: 1,
+  //       },
+
+  //     })
+  //     @ApiResponse({
+  //       status: 200,
+  //       description: 'The recent episodes have been successfully fetched.',
+  //       type: [FetchEpisodeResponseDto],
+  //     })
+  //     @ApiResponse({
+  //       status: 400,
+  //       description: 'Invalid page parameter',
+  //       type: ValidationErrorResponseDto,
+  //     })
+  //     @ApiResponse({
+  //       status: 404,
+  //       description: 'Episodes not found',
+  //       type: NotFoundErrorResponseDto,
+  //     })
   async fetchRecentEpisodes(
     @Query('page', queryValidationPipe) page: PageQueryParams,
   ) {
