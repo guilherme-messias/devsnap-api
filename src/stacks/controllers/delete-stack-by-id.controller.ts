0bfc657daeab615831e358b130cc1da140ae7ecf
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-22 12:34:04 -0300

    feat: add controller to delete a stack by ID with validation and error handling

---
+++ src/stacks/controllers/delete-stack-by-id.controller.ts
@@ -0,0 +1,61 @@
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  Param,
+  NotFoundException,
+} from '@nestjs/common';
+import { ApiTags, ApiOperation, ApiParam, ApiResponse } from '@nestjs/swagger';
+import z from 'zod';
+import { NotFoundErrorResponseDto } from '../../episodes/controllers/schemas/response/not-found-error.response.schema';
+import { ValidationErrorResponseDto } from '../../http/schemas/response/validation-error.response.schema';
+import { ZodValidationPipe } from '../../pipes/ZodValidationPipe';
+import { DeleteStackByIdService } from '../services/delete-stack-by-id.service';
+
+const idParamSchema = z.uuid();
+
+const paramValidationPipe = new ZodValidationPipe(idParamSchema);
+type IdParam = z.infer<typeof idParamSchema>;
+
+@ApiTags('stacks')
+@Controller('/stacks')
+export class DeleteStackByIdController {
+  constructor(
+    private readonly deleteStackByIdService: DeleteStackByIdService,
+  ) {}
+
+  @Delete(':id')
+  @HttpCode(204)
+  @ApiOperation({
+    summary: 'Delete a stack by ID',
+  })
+  @ApiParam({
+    name: 'id',
+    description: 'Stack ID',
+    required: true,
+    format: 'uuid',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'The stack has been successfully deleted.',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid ID parameter',
+    type: ValidationErrorResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Stack not found',
+    type: NotFoundErrorResponseDto,
+  })
+  async deleteStackById(@Param('id', paramValidationPipe) id: IdParam) {
+    const deletedStack = await this.deleteStackByIdService.deleteStackById(id);
+
+    if (!deletedStack) {
+      throw new NotFoundException(`Stack with ID ${id} not found`);
+    }
+
+    return;
+  }
+}
