8e14e58dc5c848cbc032c5e55873d7524c9a22f9
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-22 13:02:17 -0300

    feat: add FetchRecentStacksService for fetching recent stacks with pagination

---
+++ src/stacks/services/fetch-recent-stacks.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class FetchRecentStacksService {
+  constructor(private prisma: PrismaService) {}
+
+  async fetchRecentStacks(data: { page: number; perPage: number }) {
+    const { page, perPage } = data;
+
+    const stacks = await this.prisma.stack.findMany({
+      skip: (page - 1) * perPage,
+      take: perPage,
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return stacks;
+  }
+}
