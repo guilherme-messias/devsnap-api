38f500f03c0062dbc2ab20aaf06ba5bb02a7412e
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-22 12:44:23 -0300

    feat: implement FetchStackByIdService to retrieve stack by ID

---
+++ src/stacks/services/fetch-stack-by-id.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../../prisma/prisma.service';
+
+@Injectable()
+export class FetchStackByIdService {
+  constructor(private readonly prisma: PrismaService) {}
+  async fetchStackById(id: string) {
+    const stack = await this.prisma.stack.findUnique({
+      where: { id },
+    });
+
+    return stack;
+  }
+}
