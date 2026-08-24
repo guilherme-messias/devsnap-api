7918fd6284cc58f361491d1c2404d211cf7ac3b7
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-24 08:47:22 -0300

    feat: add delete annotations service

---
+++ src/modules/annotations/services/delete-annotations-by-id.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@infrastructure/prisma/prisma.service';
+
+@Injectable()
+export class DeleteAnnotationsByIdService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async deleteAnnotationsById(id: string) {
+    const annotation = await this.prisma.annotation.findUnique({
+      where: { id },
+    });
+
+    if (!annotation) {
+      return null;
+    }
+
+    const deletedAnnotation = await this.prisma.annotation.delete({
+      where: { id },
+    });
+
+    return deletedAnnotation;
+  }
+}
