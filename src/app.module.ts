8b53547cb8ff4d5c83e63c653fc4c9d94bd83193
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-26 18:05:03 -0300

    feat: refactor episode review module structure and implement new controller, service, and schemas

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { EpisodesModule } from './modules/episodes/episodes.module';
 import { PrismaModule } from './infrastructure/prisma/prisma.module';
 import { StacksModule } from './modules/stacks/stacks.module';
 import { AnnotationsModule } from './modules/annotations/annotations.module';
-import { EpisodeReviewModule } from './modules/episode-review/episode-review.module';
+import { EpisodeReviewModule } from './modules/episodes-review/episode-review.module';
 
 @Module({
   imports: [
