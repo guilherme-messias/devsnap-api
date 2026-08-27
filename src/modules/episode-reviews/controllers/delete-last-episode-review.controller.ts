5b4c1d906b523d8da8dcfb897bef0b36fe637ad3
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-27 14:27:18 -0300

    feat: add DeleteLastEpisodeReview controller and service to episode reviews module

---
+++ src/modules/episode-reviews/controllers/delete-last-episode-review.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Controller,
+  Delete,
+  HttpCode,
+  NotFoundException,
+  Param,
+} from '@nestjs/common';
+import { ZodValidationPipe } from '@src/shared/pipes/ZodValidationPipe';
+import z from 'zod';
+import { DeleteLastEpisodeReviewService } from '../services/delete-last-episode-review.service';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { EpisodeReviewOrEpisodeNotFoundErrorResponseDto } from '@src/shared/http/schemas/response/episode-review-or-episode-not-found-error.response.schema';
+import { ValidationErrorResponseDto } from '@src/shared/http/schemas/response/validation-error.response.schema';
+
+const episodeIdSchema = z.uuid();
+type EpisodeId = z.infer<typeof episodeIdSchema>;
+
+@ApiTags('episode-reviews')
+@Controller('/episodes')
+export class DeleteLastEpisodeReviewController {
+  constructor(
+    private readonly deleteLastEpisodeReviewService: DeleteLastEpisodeReviewService,
+  ) {}
+
+  @Delete(':episodeId/reviews/latest')
+  @HttpCode(204)
+  @ApiOperation({
+    summary: 'Delete the last episode review',
+  })
+  @ApiParam({
+    name: 'episodeId',
+    description: 'Episode ID',
+    required: true,
+    format: 'uuid',
+  })
+  @ApiResponse({
+    status: 204,
+    description: 'The last episode review has been deleted',
+  })
+  @ApiResponse({
+    status: 400,
+    description: 'Invalid episode ID',
+    type: ValidationErrorResponseDto,
+  })
+  @ApiResponse({
+    status: 404,
+    description: 'Episode review or episode not found',
+    type: EpisodeReviewOrEpisodeNotFoundErrorResponseDto,
+  })
+  async deleteLastEpisodeReview(
+    @Param('episodeId', new ZodValidationPipe(episodeIdSchema))
+    episodeId: EpisodeId,
+  ) {
+    const deletedEpisodeReview =
+      await this.deleteLastEpisodeReviewService.deleteLastEpisodeReview(
+        episodeId,
+      );
+
+    if (!deletedEpisodeReview) {
+      throw new NotFoundException('Episode review or episode not found');
+    }
+
+    return;
+  }
+}
--- src/modules/episode-reviews/episode-reviews.module.ts
+++ src/modules/episode-reviews/episode-reviews.module.ts
@@ -6,18 +6,22 @@ import { FetchEpisodeReviewByIdController } from './controllers/fetch-episode-re
 import { FetchEpisodeReviewByIdService } from './services/fetch-episode-review-by-id.service';
 import { FetchRecentEpisodeReviewsController } from './controllers/fetch-recent-episode-reviews.controller';
 import { FetchRecentEpisodeReviewsService } from './services/fetch-recent-episode-reviews.service';
+import { DeleteLastEpisodeReviewController } from './controllers/delete-last-episode-review.controller';
+import { DeleteLastEpisodeReviewService } from './services/delete-last-episode-review.service';
 
 @Module({
   controllers: [
     CreateEpisodeReviewController,
     FetchEpisodeReviewByIdController,
     FetchRecentEpisodeReviewsController,
+    DeleteLastEpisodeReviewController,
   ],
   imports: [PrismaModule],
   providers: [
     CreateEpisodeReviewService,
     FetchEpisodeReviewByIdService,
     FetchRecentEpisodeReviewsService,
+    DeleteLastEpisodeReviewService,
   ],
 })
 export class EpisodeReviewsModule {}
