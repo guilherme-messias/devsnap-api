720657a904f3fb185c3f2acca6be77952b06dc26
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-27 14:03:14 -0300

    feat: add delete last episode review service

---
+++ src/modules/episode-reviews/services/delete-last-episode-review.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@src/infrastructure/prisma/prisma.service';
+
+@Injectable()
+export class DeleteLastEpisodeReviewService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async deleteLastEpisodeReview(episodeId: string) {
+    const lastEpisodeReview = await this.prisma.episodeReview.findFirst({
+      where: { episodeId },
+      orderBy: { reviewAt: 'desc' },
+    });
+
+    if (!lastEpisodeReview) {
+      return null;
+    }
+
+    return this.prisma.episodeReview.delete({
+      where: { id: episodeId },
+    });
+  }
+}
