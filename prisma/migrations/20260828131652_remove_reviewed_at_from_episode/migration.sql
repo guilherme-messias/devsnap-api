95a8ce175dcd2d7ae377f01d4198d932a82c155f
Guilherme Messias <messiasguilherme700@gmail.com>
2026-08-28 10:17:10 -0300

    refactor: remove reviewed field from Episode model in schema.prisma

---
+++ prisma/migrations/20260828131652_remove_reviewed_at_from_episode/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `reviewed` on the `episodes` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "episodes" DROP COLUMN "reviewed";
