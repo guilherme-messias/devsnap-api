41c498e5e1249c88bd816a0b240ca3b477d93e48
Guilherme Messias <messiasguilherme700@gmail.com>
2026-09-01 10:19:57 -0300

    feat: integrate @nestjs/jwt and @nestjs/passport modules for enhanced JWT authentication support in the application

--- src/infrastructure/jwt/jwt.module.ts
+++ src/infrastructure/jwt/jwt.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
 import { JwtService } from './jwt.service';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule as JwtNestModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtNestModule.registerAsync({
+      useFactory: async (configService: ConfigService) => ({
+        publicKey: configService.get('JWT_PUBLIC_KEY'),
+        privateKey: configService.get('JWT_PRIVATE_KEY'),
+        signOptions: { expiresIn: configService.get('JWT_EXPIRES_IN') },
+        //TODO: adicionar return
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   providers: [JwtService],
   exports: [JwtService],
 })
