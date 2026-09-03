ae18dfcc25dcdfdaf14efa2bbb9e125ffb0c6d20
Guilherme Messias <messiasguilherme700@gmail.com>
2026-09-03 09:38:36 -0300

    feat: implement AuthService with token generation and refresh token hashing for enhanced user authentication

--- src/infrastructure/auth/auth.service.ts
+++ src/infrastructure/auth/auth.service.ts
@@ -1,4 +1,44 @@
 import { Injectable } from '@nestjs/common';
-
+import { PrismaService } from '../prisma/prisma.service';
+import { JwtService } from '@nestjs/jwt';
+import argon2 from 'argon2';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+  ) {}
+
+  async generateToken(userId: string, email: string) {
+    const payload = { sub: userId, email };
+    const [accessToken, refreshToken] = await Promise.all([
+      this.jwt.signAsync(payload, {
+        privateKey: process.env.JWT_PRIVATE_KEY,
+        algorithm: 'RS256',
+        expiresIn: '15m',
+      }),
+      this.jwt.signAsync(payload, {
+        privateKey: process.env.JWT_PRIVATE_KEY,
+        algorithm: 'RS256',
+        expiresIn: '7d',
+      }),
+    ]);
+    return { accessToken, refreshToken };
+  }
+
+  async updateRefreshTokenHash(userId: string, refreshToken: string | null) {
+    if (!refreshToken) {
+      await this.prisma.user.update({
+        where: { id: userId },
+        data: { hashedRefreshToken: null },
+      });
+      return;
+    }
+
+    const hash = await argon2.hash(refreshToken);
+    await this.prisma.user.update({
+      where: { id: userId },
+      data: { hashedRefreshToken: hash },
+    });
+  }
+}
